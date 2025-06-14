1a6061a512a27b22ace6bf30cacf0e2b570e44fa
ABDELHADI EL BCIR <105753053+abdelhadi-elbcir@users.noreply.github.com>
2025-06-14 15:52:03 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ function App() {
             <ul className="menu-list">
               {category.menuItems.map((item) => (
                 <li key={item.id} className="menu-item">
-                  <img src={`https://srv796999.hstgr.cloud/backend/api/images/${it.imageUrl}`} alt={item.name} className="menu-image" />
+                  <img src={`https://srv796999.hstgr.cloud/backend/api/images/${item.imageUrl}`} alt={item.name} className="menu-image" />
                   <div className="menu-details">
                     <h3 className="menu-name">{item.name}</h3>
                     <p className="menu-description">{item.description}</p>
